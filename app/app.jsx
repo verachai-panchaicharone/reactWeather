b118484ea664fc0996423a45a40fff7419461a9c
Verachai Panchaicharone <petersaint2000@gmail.com>
2017-03-07 19:01:17 +0700

    Add Main.jsx with Router

--- app/app.jsx
+++ app/app.jsx
@@ -1,19 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var objOne = {
-  name: 'Bong',
-  location: 'Bangkok'
-};
-
-var objTwo = {
-  age: 25,
-  ...objOne
-};
-
-console.log(objTwo);
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
 
 ReactDOM.render(
-    <h1>boilerplate !</h1>,
+    <Router history={hashHistory}>
+      <Route path="/" component={Main}>
+
+      </Route>
+    </Router>,
     document.getElementById('app')
   );
