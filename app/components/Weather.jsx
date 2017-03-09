5dcbb87d194135b2da0168b877ddef32744a10fb
Verachai Panchaicharone <petersaint2000@gmail.com>
2017-03-09 18:26:52 +0700

    Adding Loading Text

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -6,30 +6,40 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
   getInitialState: function () {
     return {
-      location: 'Miami',
-      temp: 88
+      isLoading: false
     }
   },
   handleSearch: function(location) {
     var that = this;
+    this.setState({ isLoading: true });
 
     openWeatherMap.getTemp(location).then(function (temp){
       that.setState({
         location: location,
-        temp: temp
+        temp: temp,
+        isLoading: false
       });
     }, function(errMessage) {
+      that.setState({ isLoading: false });
       alert(errMessage);
     });
   },
   render: function() {
-    var {temp, location} = this.state;
+    var {isLoading, temp, location} = this.state;
+
+    function renderMessage() {
+      if(isLoading) {
+        return <h3>Fetching weathher...</h3>;
+      } else if(temp && location) {
+        return <WeatherMessage temp={temp} location={location} />;
+      }
+    }
 
     return(
       <div>
         <h3>Weather Component</h3>
         <WeatherForm onSearch={this.handleSearch} />
-        <WeatherMessage location={location} temp={temp} />
+        {renderMessage()}
       </div>
     );
   }
